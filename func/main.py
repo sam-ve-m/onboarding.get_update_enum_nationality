6ab176b749c54441d6ac5daf68ed36e536fafd18
João Bonfim <jpa@lionx.com.br>
2022-05-24 15:13:22 -0300

    feat: Added tests for the service and repository of enums.
    Added tests and reformatted files with Black.

--- func/main.py
+++ func/main.py
@@ -4,7 +4,9 @@ from pathlib import Path
 
 from src.infrastructure.env_config import Configuration
 
-Configuration.get_config(env_path=Path(__file__).parent.absolute())  # This line is important to load the environment variables needed by the project!
+Configuration.get_config(
+    env_path=Path(__file__).parent.absolute()
+)  # This line is important to load the environment variables needed by the project!
 
 from src.core.response_model.response_model import ResponseModel
 from src.service.enum.service import EnumService
@@ -13,7 +15,6 @@ from src.service.enum.service import EnumService
 def get_enums(request_=request) -> Response:
     service_response = EnumService.get_response()
     response = ResponseModel.build_http_response(
-        response_model=service_response,
-        status=HTTPStatus.OK
+        response_model=service_response, status=HTTPStatus.OK
     )
     return response
--- tests/src/service/enum/test_service.py
+++ tests/src/service/enum/test_service.py
@@ -4,26 +4,35 @@ import pytest
 from src.service.enum.service import EnumService
 from src.repository.enum.repository import EnumRepository
 
-from tests.test_doubles.doubles import enum_service_get_enums_response_ok, enum_service_get_enums_response_invalid, enum_service_get_enums_response_none
-from tests.test_doubles.doubles import enum_service_response_ok, enum_service_response_none, enum_service_response_invalid
+from tests.test_doubles.doubles import (
+    enum_service_get_enums_response_ok,
+    enum_service_get_enums_response_invalid,
+    enum_service_get_enums_response_none,
+)
+from tests.test_doubles.doubles import (
+    enum_service_response_ok,
+    enum_service_response_invalid,
+    enum_service_response_none,
+)
 
 
-@patch.object(EnumRepository, 'get_enums')
+@patch.object(EnumRepository, "get_enums")
 def test_get_response_when_enums_are_ok(get_enums_mock):
     get_enums_mock.return_value = enum_service_get_enums_response_ok
     result = EnumService.get_response()
     assert result == enum_service_response_ok
 
 
-@patch.object(EnumRepository, 'get_enums')
+@patch.object(EnumRepository, "get_enums")
 def test_get_response_when_enums_are_none(get_enums_mock):
     get_enums_mock.return_value = enum_service_get_enums_response_none
     result = EnumService.get_response()
     assert result == enum_service_response_none
 
 
-@patch.object(EnumRepository, 'get_enums')
+@patch.object(EnumRepository, "get_enums")
 def test_get_response_when_enums_are_invalid(get_enums_mock):
-    get_enums_mock.return_value = enum_service_get_enums_response_invalid
-    result = EnumService.get_response()
-    assert result == enum_service_response_invalid
+    get_enums_mock.side_effect = Exception("Erroooooou!")
+    with pytest.raises(Exception) as error:
+        result = EnumService.get_response()
+        assert result == enum_service_response_invalid
