c770b53e5bb308f6c7c52a4244d79a9b3c8a6edf
João Bonfim <joao.bonfim@lionx.com.br>
2023-01-11 08:55:50 -0300

    chore: new image version

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 fission spec init
-fission env create --spec --name onb-br-enum-nationality-env --image nexus.sigame.com.br/fission-onboarding-br-enum-nationality:0.1.0 --poolsize 0 --version 3 --imagepullsecret "nexus-v3" --spec
+fission env create --spec --name onb-br-enum-nationality-env --image nexus.sigame.com.br/fission-onboarding-br-enum-nationality:0.2.0-0 --poolsize 0 --version 3 --imagepullsecret "nexus-v3" --spec
 fission fn create --spec --name onb-br-enum-nationality-fn --env onb-br-enum-nationality-env --code fission.py --targetcpu 80 --executortype newdeploy --maxscale 3 --requestsperpod 10000 --spec
 fission route create --spec --name onb-br-enum-nationality-rt --method GET --url /enum/nationality --function onb-br-enum-nationality-fn
